7836ace3cbd05a84352e3e78152a0ea7169c000b
MarkevichAndrey <andrey.alex.markevich@gmail.com>
2023-03-19 23:06:03 +0300

    first coommit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import TodoForm from './components/Todos/TodoForm'
+import TodoList from './components/Todos/TodoList'
+import TodosActions from './components/Todos/TodosActions'
+
+import './App.css'
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodoHandler = (text) => {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo }
+      )
+    )
+  }
+  const resetTodosHandler = () => {
+    setTodos([])
+  }
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted))
+  }
+  const completedTodoCount = todos.filter((todo) => todo.isCompleted).length
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      {todos.length > 0 && (
+        <TodosActions
+          completedTodoExist={!!completedTodoCount}
+          resetTodos={resetTodosHandler}
+          deleteCompletedTodos={deleteCompletedTodosHandler}
+        />
+      )}
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
+      {completedTodoCount > 0 && (
+        <h2>
+          {`You have completed ${completedTodoCount} ${
+            completedTodoCount > 1 ? 'todos' : 'todo'
+          }`}
+        </h2>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,27 @@
+import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri'
+import { FaCheck } from 'react-icons/fa'
+import styles from './Todo.module.css'
+
+function Todo({ todo, deleteTodo, toggleTodo }) {
+  return (
+    <div
+      className={`${styles.todo} ${
+        todo.isCompleted ? styles.completedTodo : ''
+      }`}
+    >
+      <RiTodoFill className={styles.todoIcon} />
+      <div className={styles.todoText}>{todo.text}</div>
+      <div></div>
+      <RiDeleteBin2Line
+        className={styles.deleteIcon}
+        onClick={() => deleteTodo(todo.id)}
+      />
+      <FaCheck
+        className={styles.checkIcon}
+        onClick={() => toggleTodo(todo.id)}
+      />
+    </div>
+  )
+}
+
+export default Todo
